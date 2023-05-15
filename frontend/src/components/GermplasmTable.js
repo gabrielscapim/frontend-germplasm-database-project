7aaa91e701efd71dc76733bd10101910713cfc07
Gabriel <scapimgabriel@gmail.com>
2023-05-15 17:32:27 -0300

    feat: fetch api test

--- frontend/src/components/GermplasmTable.js
+++ frontend/src/components/GermplasmTable.js
@@ -1,11 +1,42 @@
+// import axios from 'axios';
+// import { useEffect, useState } from 'react';
 import styles from './GermplasmTable.module.css';
 
 function GermplasmTable() {
+  // const [apiResults, setApiResults] = useState([]);
+  // const fetchApi = async () => {
+  //   const response = await fetch('http://localhost:8080/api/germplasm');
+  //   const data = await response.json();
+  //   return data;
+  // };
+
+  // useEffect(() => {
+  //   fetchApi().then((response) => {
+  //     setApiResults(Object.keys(response[0]));
+  //   });
+  // }, []);
+
+  // console.log(apiResults);
+
+  // axios.post('http://localhost:8080/api/employees', {
+  //   firstName: 'Fred',
+  //   lastName: 'Flintstone',
+  // })
+  //   .then((response) => {
+  //     console.log(response);
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
+
   return (
     <div className={ styles['table-wrapper'] }>
       <table className="table">
         <thead>
           <tr>
+            {/* { apiResults.map((result, index) => (
+              <th scope="col" key={ index }>{ result }</th>
+            )) } */}
             <th scope="col">#</th>
             <th scope="col">First</th>
             <th scope="col">Last</th>
@@ -15,9 +46,21 @@ function GermplasmTable() {
         <tbody>
           <tr>
             <th scope="row">1</th>
-            <td>MarsdfsdfsdfsddasdasdasdasdSDsfsdfsdfsdffdsk</td>
-            <td>Otto</td>
-            <td>@mdo</td>
+            <td>linhagem1</td>
+            <td>3</td>
+            <td>2</td>
+            <td>2</td>
+            <td>9</td>
+            <td>1</td>
+          </tr>
+          <tr>
+            <th scope="row">2</th>
+            <td>linhagem2</td>
+            <td>5</td>
+            <td>2</td>
+            <td>4</td>
+            <td>1</td>
+            <td>2</td>
           </tr>
         </tbody>
       </table>
