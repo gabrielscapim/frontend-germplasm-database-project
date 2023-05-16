d762c5b79586f3a69e563a669eae6ba16bc46631
Gabriel <scapimgabriel@gmail.com>
2023-05-16 10:44:47 -0300

    style: header

--- frontend/src/components/ConsultGermplasms/Filters.js
+++ frontend/src/components/ConsultGermplasms/Filters.js
@@ -94,42 +94,80 @@ function Filters({ attributes }) {
         </div>
       ) }
       { window.innerWidth < MAX_SCREEN_SIZE && (
-        <div className={ styles['filters-container-third-line'] }>
-          <Datalist
-            id="column-filter"
-            label="Coluna"
-            options={ attributes }
-            name="columnOrderState"
-          />
-          <div className={ styles['filters-radios-container'] }>
-            <Input
-              id="column-sort-input-asc"
-              label="Ascendente"
-              type="radio"
-              inputValue="ASC"
-              name="sortState"
+        <>
+          <div className={ styles['filters-container-second-line'] }>
+            <Datalist
+              id="column-filter"
+              label="Coluna"
+              options={ attributes }
+              name="columnFilter"
+            />
+            <Select
+              id="comparison-filter"
+              label="Operador"
+              options={ [
+                'maior que',
+                'menor que',
+                'igual a',
+              ] }
+              name="comparisonFilter"
             />
             <Input
-              id="column-sort-input-desc"
-              label="Descendente"
-              type="radio"
-              inputValue="DESC"
-              name="sortState"
-              containerClassName="radio-container"
+              id="value-filter"
+              label="Valor"
+              type="number"
+              name="valueFilter"
+              labelClassName="general-input-label"
+              inputClassName="value-input"
+              componentStyles={ {
+                marginRight: '2%',
+              } }
+            />
+            <Button
+              id="button-filter"
+              label="Filtrar"
+              type="button"
+              disabled={ false }
+              componentStyles={ purpleButtonStyles }
             />
           </div>
-          <Button
-            id="column-sort-button"
-            label="Ordenar"
-            type="button"
-            disabled={ false }
-            componentStyles={ {
-              backgroundColor: '#684f92',
-              border: '1px solid #684f92',
-              marginRight: '1%',
-            } }
-          />
-        </div>
+          <div className={ styles['filters-container-third-line'] }>
+            <Datalist
+              id="column-filter"
+              label="Coluna"
+              options={ attributes }
+              name="columnOrderState"
+            />
+            <div className={ styles['filters-radios-container'] }>
+              <Input
+                id="column-sort-input-asc"
+                label="Ascendente"
+                type="radio"
+                inputValue="ASC"
+                name="sortState"
+              />
+              <Input
+                id="column-sort-input-desc"
+                label="Descendente"
+                type="radio"
+                inputValue="DESC"
+                name="sortState"
+                containerClassName="radio-container"
+              />
+            </div>
+            <Button
+              id="column-sort-button"
+              label="Ordenar"
+              type="button"
+              disabled={ false }
+              componentStyles={ {
+                backgroundColor: '#684f92',
+                border: '1px solid #684f92',
+                marginRight: '1%',
+              } }
+            />
+          </div>
+        </>
       ) }
       <Button
         id="button-filter"
