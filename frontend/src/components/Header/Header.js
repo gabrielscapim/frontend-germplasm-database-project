9e2544d04e4e1f370e4db25b3ed261bb9da85748
Gabriel <scapimgabriel@gmail.com>
2023-06-07 14:50:15 -0300

    style: header

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,7 +1,14 @@
-/* eslint-disable max-len */
 import styles from './Header.module.css';
 
 function Header() {
+  const headerOptionDecoration = {
+    textDecoration: 'underline',
+    textUnderlineOffset: '5px',
+    textDecorationColor: '#faf9f6',
+  };
+  const location = window.location.href;
+  console.log(location);
+
   return (
     <header className={ styles.header }>
       <div className={ styles['header-title-options'] }>
@@ -10,12 +17,14 @@ function Header() {
           <a
             href="/consult-germplasms"
             className={ styles['nav-option'] }
+            style={ location.includes('consult') ? headerOptionDecoration : {} }
           >
             Consultar banco
           </a>
           <a
             href="/add-germplasm"
             className={ styles['nav-option'] }
+            style={ location.includes('add') ? headerOptionDecoration : {} }
           >
             Adicionar novo
           </a>
