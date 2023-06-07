fcaab800b3d6dbd717bcb94dc8b29d5c00c494c2
Gabriel <scapimgabriel@gmail.com>
2023-06-07 15:25:26 -0300

    feat: exit button

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,3 +1,4 @@
+import { useNavigate } from 'react-router-dom';
 import styles from './Header.module.css';
 
 function Header() {
@@ -7,27 +8,45 @@ function Header() {
     textDecorationColor: '#faf9f6',
   };
   const location = window.location.href;
-  console.log(location);
+  const navigate = useNavigate();
+
+  const exitClick = () => {
+    if (window.confirm('Deseja sair da aplicação?')) {
+      localStorage.removeItem('token');
+      localStorage.removeItem('tokenGenerationTime');
+      navigate('/login');
+    }
+  };
 
   return (
     <header className={ styles.header }>
       <div className={ styles['header-title-options'] }>
         <h3>Germplasm Database</h3>
         <nav>
-          <a
-            href="/consult-germplasms"
-            className={ styles['nav-option'] }
-            style={ location.includes('consult') ? headerOptionDecoration : {} }
+          <div>
+            <a
+              href="/consult-germplasms"
+              // eslint-disable-next-line sonarjs/no-duplicate-string
+              className={ styles['nav-option'] }
+              style={ location.includes('consult') ? headerOptionDecoration : {} }
+            >
+              Consultar banco
+            </a>
+            <a
+              href="/add-germplasm"
+              className={ styles['nav-option'] }
+              style={ location.includes('add') ? headerOptionDecoration : {} }
+            >
+              Adicionar novo
+            </a>
+          </div>
+          <button
+            type="button"
+            onClick={ exitClick }
+            className={ styles['button-option'] }
           >
-            Consultar banco
-          </a>
-          <a
-            href="/add-germplasm"
-            className={ styles['nav-option'] }
-            style={ location.includes('add') ? headerOptionDecoration : {} }
-          >
-            Adicionar novo
-          </a>
+            Sair
+          </button>
         </nav>
       </div>
       {/* <div className={ styles['user-container'] }>
