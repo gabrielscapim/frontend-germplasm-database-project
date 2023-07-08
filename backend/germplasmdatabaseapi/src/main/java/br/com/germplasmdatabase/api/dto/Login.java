81bfcbbe59a7313af91b9c8abe4c2e971144571f
Gabriel <scapimgabriel@gmail.com>
2023-07-08 15:32:38 -0300

    refactor: api será realocada em outro repositório, assim como sua refatoração

--- backend/germplasmdatabaseapi/src/main/java/br/com/germplasmdatabase/api/dto/Login.java
+++
@@ -1,4 +0,0 @@
-package br.com.germplasmdatabase.api.dto;
-
-public record Login(String login, String password) {
-}
--- backend/germplasmdatabaseapi/src/main/java/br/com/germplasmdatabase/api/model/repositories/GermplasmRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.germplasmdatabase.api.model.repositories;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.germplasmdatabase.api.model.entities.Germplasm;
-
-public interface GermplasmRepository extends JpaRepository<Germplasm, Integer> {
-
-}
